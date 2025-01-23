fba46ee51aab61ac930225a82efba828215d8c16
yoonad16 <ellajya@naver.com>
2025-01-24 02:21:21 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+long long n;
+long long count = 0;
+
+int main(){
+    ios:: sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    count = n*n;
+    
+    cout << count << '\n';
+    cout << 2;
+}
